4a43170acee24b64c941d3fcc5395084210fbf92
ahmedali762 <ahmed.spidy444@gmail.com>
2021-07-10 14:17:17 +0500

    java2

---
+++ app.js
@@ -0,0 +1,160 @@
+// var first_name = prompt('enter first name')
+// var second_name = prompt('enter second name')
+
+// alert('welcome'+first_name+second_name)
+
+// var fav_phone = prompt('enter favorite mobile model')
+
+// console.log('my favorite phone is '+fav_phone)
+// console.log(indexOf(fav_phone))
+
+
+// var a = 'pakistan'
+// for(i=0; i < a.length ; i++){
+//     if(i==a){
+
+// console.log(a.indexOf('n'))
+
+//     }
+
+// }
+
+
+// var a = 'hello world'
+
+// console.log(a.indexOf('l'))
+
+
+// var a = 'hyderabad'
+
+// console.log(a.replace('hyder','islam'))
+
+
+// var message = “Ali and Sami are best friends. They play cricket and football together.”
+
+// console.log(a.replace('and','&'))
+
+
+// var str = "472"
+
+// number = parseInt(str)
+
+// number = Number(str)
+
+// var a = prompt('enter word')
+
+// console.log(a.toUpperCase())
+
+
+// var a = 'javascript'
+
+// console.log(a.slice(0,1).toUpperCase()+ a.slice(1))
+
+
+
+// var num = 35.36 ;
+
+// console.log(num.toLocaleString())
+
+
+
+// var item = prompt('enter item')
+// item = item.slice(0,1).toUpperCase() + item.slice(1).toLowerCase()
+
+// var arr = ['cake', 'apple pie', 'cookie', 'chips', 'patties']
+
+
+
+
+// for(var i=0; i<arr.length; i++){
+
+//     if(arr[i] == item ){
+       
+//        console.log('found')
+       
+//     }
+//     else {
+//         console.log('Not found')  
+//     }
+// }
+
+
+
+// var int =+ prompt('enter positive number')
+
+//         console.log(int)
+//         console.log(Math.round(int))
+//         console.log(Math.floor(int))
+//         console.log(Math.ceil(int))
+
+// var int =+ prompt('enter negitive number')
+
+//         console.log(int)
+//         console.log(Math.round(int))
+//         console.log(Math.floor(int))
+//         console.log(Math.ceil(int))
+
+
+// var num = -7 
+// console.log(Math.abs(num))
+
+
+
+// var num = Math.random()*6
+
+// console.log(Math.ceil(num))
+
+
+
+
+// var coin = Math.ceil(Math.random()*2)
+
+// if (coin == 1) {
+// console.log('heads')
+    
+// } else {
+// console.log('tails')
+    
+// }
+
+
+
+
+// var num = Math.random()*100
+
+// console.log( 'random number between 1 and 100 : ' + Math.ceil(num))
+
+
+
+// var weight = prompt('enter your weight')
+
+// console.log('your weight is : ' + parseFloat(weight) + 'kgs')
+
+
+
+// var scNum = 8
+
+// var num = prompt('enter secret number')
+
+// if (num == scNum) {
+//     console.log('congrats you found the secret num ')
+// } 
+// else{
+//     console.log('try again')  
+// }
+
+
+
+// var a = new Date()
+// console.log(a)
+
+
+// var a = new Date()
+
+// var month = ['january' , 'fabuary' , 'march' , 'april' , 'may' , 'june' , 'july' , 'august' , 'september' , 'october' , 'november' , 'december']
+
+// alert(month.getMonth())
+
+// var a = new Date()
+// var days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+// console.log(a.getDay()) 
